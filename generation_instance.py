b207f92b1e61692592fb955fba22c4049fe2cffb
Zahra1903 <Zahra.Teibi@etu.sorbonne-universite.fr>
2026-03-24 15:15:19 +0100

    GI

--- generation_instance.py
+++ generation_instance.py
@@ -9,51 +9,51 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-n = 994     # Nombre de candidats (pr condorcet maximum recursion depth exceeded pour n>996 et pour 3/4 994)
-m=50 #nombre de votants
-l_thetas = [0.00001, 0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
-s0 = np.arange(0,n) # Classement initial [0, 1, 2, ..., 9]
+ns = [50,100,500,994]     # Nombre de candidats (pr condorcet maximum recursion depth exceeded pour n>996 et pour 3/4 994)
+m=1000 #nombre de votants
+l_thetas = [0.00001, 0.0001,0.001,0.01,0.1,0.5,0.8,1, 2, 3] # aller jusqu'a 3 et faire plus de point entre 0 et 0.1 ; plus on avance plus on espace les evals
 red = [instance.majority_trois_quart, instance.condorcet_etendu]
 
-f = open("eval_generation.csv",'a')
+f = open("eval_generation_reduction.csv",'a')
 writer = csv.writer(f)
-writer.writerow(['theta','taille_max_3/4','taille_max_CCE'])
+writer.writerow(['N','theta','taille_max_3/4','taille_max_CCE'])
+for n in ns : 
+    s0 = np.arange(0,n) # Classement initial [0, 1, 2, ..., 9]
+    for theta in l_thetas : # Paramètre de dispersion si theta proche de 0 => proba uniforme si theta proche de 1 => loi certaine de P
 
-for theta in l_thetas : # Paramètre de dispersion si theta proche de 0 => proba uniforme si theta proche de 1 => loi certaine de P
+        for i in range(50) : #tester avec plusieurs profils differents
+            # Générer un profil de préférence pour m votants et n candidats
+            profil_preferences = mk.sample(m=m, n=n, theta=theta, s0=s0)
 
-    for i in range(50) : #tester avec plusieurs profils differents
-        # Générer un profil de préférence pour m votants et n candidats
-        profil_preferences = mk.sample(m=m, n=n, theta=theta, s0=s0)
+            inst = instance.Instance()
+            inst.nb_candidats = n
+            inst.nb_votants = m 
 
-        inst = instance.Instance()
-        inst.nb_candidats = n
-        inst.nb_votants = m 
+            inst.candidats = {}
+            for j in range(n):
+                inst.candidats[j+1] = str(j+1) 
 
-        inst.candidats = {}
-        for j in range(n):
-            inst.candidats[j+1] = str(j+1) 
+            inst.matPref = np.zeros((n+1,n+1)) #matPref[k,j] = nb de votants qui preferent k à j, la premiere ligne et premiere colonnes sont vides pr avoir les bon indices
+            # On boucle sur chaque vote du profil
+            for pref in profil_preferences:
+                # On parcourt toutes les paires de POSITIONS (k, j)
+                for k in range(1,n+1): 
+                    for j in range(k + 1, n+1): 
+                        # Le candidat à la position k est préféré au candidat à la position j
+                        inst.matPref[pref[k-1]+1, pref[j-1]+1] += 1
+            inst.init = True
 
-        inst.matPref = np.zeros((n+1,n+1)) #matPref[k,j] = nb de votants qui preferent k à j, la premiere ligne et premiere colonnes sont vides pr avoir les bon indices
-        # On boucle sur chaque vote du profil
-        for pref in profil_preferences:
-            # On parcourt toutes les paires de POSITIONS (k, j)
-            for k in range(1,n+1): 
-                for j in range(k + 1, n+1): 
-                    # Le candidat à la position k est préféré au candidat à la position j
-                    inst.matPref[pref[k-1], pref[j-1]] += 1
-        inst.init = True
-
-        f.write(str(theta))
-        #taille max d'une sous instance
-        for rd in red :
-            print(theta, i ,' ', rd)
-            classement = rd(inst)
-            nbmax = 0 
-            for i in classement : 
-                if type(i) == instance.Instance:
-                    nbmax = max(nbmax,i.nb_candidats)
-            f.write(',' + str(nbmax))
-        f.write('\n')
+            f.write(str(theta))
+            #taille max d'une sous instance
+            for rd in red :
+                print(theta, i ,' ', rd)
+                classement = rd(inst)
+                nbmax = 0 
+                for i in classement : 
+                    if type(i) == instance.Instance:
+                        nbmax = max(nbmax,i.nb_candidats)
+                f.write(',' + str(nbmax))
+            f.write('\n')
 
 f.close()
 type_colonne = ['taille_max_3/4','taille_max_CCE']
